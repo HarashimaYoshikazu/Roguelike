c1624c155987025d8cfadc5f63a1ff47af03f66a
Harashima <harashima318@gmail.com>
2022-06-02 00:51:40 +0900

    [add]UniRX導入

---
+++ Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    [SerializeField,Tooltip("敵のプレハブ（仮）")]
+    GameObject _enemyPrefab;
+
+
+}
