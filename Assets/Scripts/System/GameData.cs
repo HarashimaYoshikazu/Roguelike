b67ee90a2c60e2eeed747ae409c2005f056cbc29
HarashimaYoshikazu <harashima318@gmail.com>
2022-06-03 18:39:42 +0900

    [add]Singletonクラス、GameManagerクラス、GameDataクラスを追加

---
+++ Assets/Scripts/System/GameData.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// スキルの種類、種類によって後の処理を変更する
+/// </summary>
+public enum SelectType
+{
+    Skill = 1,
+    Passive = 2,
+    Execute = 3,
+}
+
+/// <summary>
+/// マスターデータ
+/// </summary>
+public class GameData
+{
+    //ToDo GSSから値を読み込む
+    static private List<int> _expTable = new List<int>()
+    {
+        1,5,10,30,70,190
+    }
+    ;
+    /// <summary>
+    /// レベルアップテーブルの読み取りプロパティ
+    /// </summary>
+    static public List<int> ExpTable => _expTable;
+}
