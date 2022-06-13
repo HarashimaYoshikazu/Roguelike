690dc55234750ff3c9128c21959485e1ba3ca777
HarashimaYoshikazu <harashima318@gmail.com>
2022-06-13 18:46:51 +0900

    [update]弾スキルクラスのUpdate処理に記述を追加

--- Assets/Scripts/InGame/Skill/BulletSkill.cs
+++ Assets/Scripts/InGame/Skill/BulletSkill.cs
@@ -6,20 +6,48 @@ public class BulletSkill : ISkill
 {
     public SkillDef SkillId => SkillDef.Bullet;
 
+    float _timer = 0f;
     float _interval = 1f;
     int _bulletValue = 1;
+
+    
     public void LevelUp()
     {
-        
+
     }
 
     public void SetUp()
     {
-        
+
     }
 
     public void Update()
     {
-        
+        _timer += Time.deltaTime;
+
+        if (_timer> _interval)
+        {
+            var enemyList = GameManager.Instance.EnemyManager.EnemyPool.ObjectList;
+            EnemyController[] targets = new EnemyController[_bulletValue];
+
+            float prevMinDis = -1;
+            foreach (var i in enemyList)
+            {
+                Vector3 vec = i.transform.position - GameManager.Instance.Player.transform.position;
+                if (prevMinDis == -1 || vec.sqrMagnitude < prevMinDis)
+                {
+                    for (int k = 1; k < _bulletValue; k++)
+                    {
+                        targets[k] = targets[k - 1];
+                    }
+                    targets[0] = i;
+                    prevMinDis = vec.sqrMagnitude;
+                }
+            }
+
+
+
+            _timer = 0;
+        }
     }
 }
